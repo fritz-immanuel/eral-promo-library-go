3bf14d7c6ad6716a76b01f48e23ae0533546c9a3
Fritz Immanuel <fritz@atomic.id>
2024-10-25 20:16:40 +0700

    Fix migrations

---
+++ databases/migrations/202410251801_insert_employee_init_data.up.sql
@@ -0,0 +1,4 @@
+INSERT INTO
+  employees (id, name, email, username, password, business_id, status_id)
+VALUES
+  (UUID(), 'Fritz', 'fritz@erajaya.com', 'fritz', 'e10adc3949ba59abbe56e057f20f883e', 1, 1);
--- databases/rice-box.go
+++ databases/rice-box.go
@@ -36,9 +36,9 @@ func init() {
 	}
 	file6 := &embedded.EmbeddedFile{
 		Filename:    "202410251404_create_table_users.up.sql",
-		FileModTime: time.Unix(1729841194, 0),
+		FileModTime: time.Unix(1729855932, 0),
 
-		Content: string("CREATE TABLE users (\r\n  id VARCHAR(255) NOT NULL,\r\n  name VARCHAR(255) NOT NULL,\r\n  email VARCHAR(255) DEFAULT '',\r\n  username VARCHAR(255) NOT NULL,\r\n  password VARCHAR(255) NOT NULL,\r\n  business_id INT NOT NULL,\r\n  \r\n  status_id VARCHAR(5) DEFAULT '1',\r\n  created_at DATETIME DEFAULT CURRENT_TIMESTAMP,\r\n  created_by VARCHAR(255) DEFAULT '',\r\n  updated_at DATETIME DEFAULT CURRENT_TIMESTAMP,\r\n  updated_by VARCHAR(255) DEFAULT '',\r\n  PRIMARY KEY (id),\r\n  INDEX idx_business_id (business_id)\r\n);"),
+		Content: string("CREATE TABLE users (\r\n  id VARCHAR(255) NOT NULL,\r\n  name VARCHAR(255) NOT NULL,\r\n  email VARCHAR(255) DEFAULT '',\r\n  username VARCHAR(255) NOT NULL,\r\n  password VARCHAR(255) NOT NULL,\r\n  \r\n  status_id VARCHAR(5) DEFAULT '1',\r\n  created_at DATETIME DEFAULT CURRENT_TIMESTAMP,\r\n  created_by VARCHAR(255) DEFAULT '',\r\n  updated_at DATETIME DEFAULT CURRENT_TIMESTAMP,\r\n  updated_by VARCHAR(255) DEFAULT '',\r\n  PRIMARY KEY (id),\r\n  INDEX idx_username (username)\r\n);"),
 	}
 	file7 := &embedded.EmbeddedFile{
 		Filename:    "202410251405_insert_users_init_data.up.sql",
@@ -71,28 +71,46 @@ func init() {
 		Content: string("CREATE TABLE user_permissions (\r\n  id VARCHAR(255) NOT NULL,\r\n  user_id VARCHAR(255) DEFAULT '',\r\n  permission_id INT DEFAULT 0,\r\n\r\n  created_at DATETIME DEFAULT CURRENT_TIMESTAMP,\r\n  created_by VARCHAR(255) DEFAULT '',\r\n  updated_at DATETIME DEFAULT CURRENT_TIMESTAMP,\r\n  updated_by VARCHAR(255) DEFAULT '',\r\n\r\n  PRIMARY KEY (id),\r\n  INDEX idx_user_id (user_id),\r\n  INDEX idx_permission_id (permission_id)\r\n);"),
 	}
 	filec := &embedded.EmbeddedFile{
-		Filename:    "202410251410_create_table_permissions.sql",
+		Filename:    "202410251410_create_table_permissions.up.sql",
 		FileModTime: time.Unix(1729842008, 0),
 
 		Content: string("CREATE TABLE permissions (\r\n  id VARCHAR(255) NOT NULL,\r\n  package VARCHAR(255) DEFAULT '',\r\n  module_name VARCHAR(255) DEFAULT '',\r\n  action_name VARCHAR(255) DEFAULT '',\r\n  display_module_name VARCHAR(255) DEFAULT '',\r\n  display_action_name VARCHAR(255) DEFAULT '',\r\n  http_method VARCHAR(255) DEFAULT '',\r\n  route VARCHAR(255) DEFAULT '',\r\n  table_name VARCHAR(255) DEFAULT '',\r\n  is_hidden INT DEFAULT 0,\r\n\r\n  created_at DATETIME DEFAULT CURRENT_TIMESTAMP,\r\n  created_by VARCHAR(255) DEFAULT '',\r\n  updated_at DATETIME DEFAULT CURRENT_TIMESTAMP,\r\n  updated_by VARCHAR(255) DEFAULT '',\r\n\r\n  PRIMARY KEY (id),\r\n  INDEX idx_package (package),\r\n  INDEX idx_module_name (module_name)\r\n);"),
 	}
 	filed := &embedded.EmbeddedFile{
-		Filename:    "202410251411_insert_permission_data_for_users.sql",
+		Filename:    "202410251411_insert_permission_data_for_users.up.sql",
 		FileModTime: time.Unix(1729842139, 0),
 
 		Content: string("INSERT INTO\r\n  `permission`(\r\n    package,\r\n    module_name,\r\n    action_name,\r\n    display_module_name,\r\n    display_action_name,\r\n    http_method,\r\n    route,\r\n    table_name,\r\n    created_at,\r\n    created_by,\r\n    updated_at,\r\n    updated_by\r\n  )\r\nVALUES\r\n  (\r\n    'Website',\r\n    'User',\r\n    'List',\r\n    'User',\r\n    'List',\r\n    'GET',\r\n    '/web/v1/users',\r\n    'users',\r\n    CURRENT_TIMESTAMP,\r\n    '0',\r\n    CURRENT_TIMESTAMP,\r\n    '0'\r\n  ), (\r\n    'Website',\r\n    'User',\r\n    'View',\r\n    'User',\r\n    'View',\r\n    'GET',\r\n    '/web/v1/users/:id',\r\n    'users',\r\n    CURRENT_TIMESTAMP,\r\n    '0',\r\n    CURRENT_TIMESTAMP,\r\n    '0'\r\n  ), (\r\n    'Website',\r\n    'User',\r\n    'Create',\r\n    'User',\r\n    'Create',\r\n    'POST',\r\n    '/web/v1/users',\r\n    'users',\r\n    CURRENT_TIMESTAMP,\r\n    '0',\r\n    CURRENT_TIMESTAMP,\r\n    '0'\r\n  ), (\r\n    'Website',\r\n    'User',\r\n    'Edit',\r\n    'User',\r\n    'Edit',\r\n    'PUT',\r\n    '/web/v1/users/:id',\r\n    'users',\r\n    CURRENT_TIMESTAMP,\r\n    '0',\r\n    CURRENT_TIMESTAMP,\r\n    '0'\r\n  ), (\r\n    'Website',\r\n    'User',\r\n    'UpdateStatus',\r\n    'User',\r\n    'Update Status',\r\n    'PUT',\r\n    '/web/v1/users/:id/status',\r\n    'users',\r\n    CURRENT_TIMESTAMP,\r\n    '0',\r\n    CURRENT_TIMESTAMP,\r\n    '0'\r\n  ), (\r\n    'Website',\r\n    'User',\r\n    'UpdatePassword',\r\n    'User',,\r\n    'Update Password',\r\n    'PUT',\r\n    '/web/v1/users/:id/password',\r\n    'users',\r\n    CURRENT_TIMESTAMP,\r\n    '0',\r\n    CURRENT_TIMESTAMP,\r\n    '0'\r\n  ), (\r\n    'Website',\r\n    'User',\r\n    'ResetPassword',\r\n    'User',,\r\n    'Reset Password',\r\n    'PUT',\r\n    '/web/v1/users/:id/reset-password',\r\n    'users',\r\n    CURRENT_TIMESTAMP,\r\n    '0',\r\n    CURRENT_TIMESTAMP,\r\n    '0'\r\n  );"),
 	}
 	filee := &embedded.EmbeddedFile{
-		Filename:    "202410251412_insert_permission_data_for_business.sql",
+		Filename:    "202410251412_insert_permission_data_for_business.up.sql",
 		FileModTime: time.Unix(1729842315, 0),
 
 		Content: string("INSERT INTO\r\n  `permission`(\r\n    package,\r\n    module_name,\r\n    action_name,\r\n    display_module_name,\r\n    display_action_name,\r\n    http_method,\r\n    route,\r\n    table_name,\r\n    created_at,\r\n    created_by,\r\n    updated_at,\r\n    updated_by\r\n  )\r\nVALUES\r\n  (\r\n    'Website',\r\n    'Business',\r\n    'List',\r\n    'Business',\r\n    'List',\r\n    'GET',\r\n    '/web/v1/business',\r\n    'business',\r\n    CURRENT_TIMESTAMP,\r\n    '0',\r\n    CURRENT_TIMESTAMP,\r\n    '0'\r\n  ), (\r\n    'Website',\r\n    'Business',\r\n    'View',\r\n    'Business',\r\n    'View',\r\n    'GET',\r\n    '/web/v1/business/:id',\r\n    'business',\r\n    CURRENT_TIMESTAMP,\r\n    '0',\r\n    CURRENT_TIMESTAMP,\r\n    '0'\r\n  ), (\r\n    'Website',\r\n    'Business',\r\n    'Create',\r\n    'Business',\r\n    'Create',\r\n    'POST',\r\n    '/web/v1/business',\r\n    'business',\r\n    CURRENT_TIMESTAMP,\r\n    '0',\r\n    CURRENT_TIMESTAMP,\r\n    '0'\r\n  ), (\r\n    'Website',\r\n    'Business',\r\n    'Edit',\r\n    'Business',\r\n    'Edit',\r\n    'PUT',\r\n    '/web/v1/business/:id',\r\n    'business',\r\n    CURRENT_TIMESTAMP,\r\n    '0',\r\n    CURRENT_TIMESTAMP,\r\n    '0'\r\n  ), (\r\n    'Website',\r\n    'Business',\r\n    'UpdateStatus',\r\n    'Business',\r\n    'Update Status',\r\n    'PUT',\r\n    '/web/v1/business/:id/status',\r\n    'business',\r\n    CURRENT_TIMESTAMP,\r\n    '0',\r\n    CURRENT_TIMESTAMP,\r\n    '0'\r\n  );"),
+	}
+	filef := &embedded.EmbeddedFile{
+		Filename:    "202410251800_create_table_employees.up.sql",
+		FileModTime: time.Unix(1729855961, 0),
+
+		Content: string("CREATE TABLE employees (\r\n  id VARCHAR(255) NOT NULL,\r\n  name VARCHAR(255) NOT NULL,\r\n  email VARCHAR(255) DEFAULT '',\r\n  username VARCHAR(255) NOT NULL,\r\n  password VARCHAR(255) NOT NULL,\r\n  \r\n  status_id VARCHAR(5) DEFAULT '1',\r\n  created_at DATETIME DEFAULT CURRENT_TIMESTAMP,\r\n  created_by VARCHAR(255) DEFAULT '',\r\n  updated_at DATETIME DEFAULT CURRENT_TIMESTAMP,\r\n  updated_by VARCHAR(255) DEFAULT '',\r\n  PRIMARY KEY (id),\r\n  INDEX idx_username (username)\r\n);"),
+	}
+	fileg := &embedded.EmbeddedFile{
+		Filename:    "202410251801_insert_employee_init_data.up.sql",
+		FileModTime: time.Unix(1729858544, 0),
+
+		Content: string("INSERT INTO\r\n  employees (id, name, email, username, password, business_id, status_id)\r\nVALUES\r\n  (UUID(), 'Fritz', 'fritz@erajaya.com', 'fritz', 'e10adc3949ba59abbe56e057f20f883e', 1, 1);"),
+	}
+	fileh := &embedded.EmbeddedFile{
+		Filename:    "202410251802_create_table_employee_permissions.up.sql",
+		FileModTime: time.Unix(1729856223, 0),
+
+		Content: string("CREATE TABLE employee_permissions (\r\n  id VARCHAR(255) NOT NULL,\r\n  employee_id VARCHAR(255) DEFAULT '',\r\n  permission_id INT DEFAULT 0,\r\n\r\n  created_at DATETIME DEFAULT CURRENT_TIMESTAMP,\r\n  created_by VARCHAR(255) DEFAULT '',\r\n  updated_at DATETIME DEFAULT CURRENT_TIMESTAMP,\r\n  updated_by VARCHAR(255) DEFAULT '',\r\n\r\n  PRIMARY KEY (id),\r\n  INDEX idx_employee_id (employee_id),\r\n  INDEX idx_permission_id (permission_id)\r\n);"),
 	}
 
 	// define dirs
 	dir1 := &embedded.EmbeddedDir{
 		Filename:   "",
-		DirModTime: time.Unix(1729842627, 0),
+		DirModTime: time.Unix(1729861965, 0),
 		ChildFiles: []*embedded.EmbeddedFile{
 			file2, // "202410251400_create_table_status.up.sql"
 			file3, // "202410251401_insert_status_data.up.sql"
@@ -104,9 +122,12 @@ func init() {
 			file9, // "202410251407_create_table_business_configs.up.sql"
 			filea, // "202410251408_create_table_user_actions.up.sql"
 			fileb, // "202410251409_create_table_user_permissions.sql"
-			filec, // "202410251410_create_table_permissions.sql"
-			filed, // "202410251411_insert_permission_data_for_users.sql"
-			filee, // "202410251412_insert_permission_data_for_business.sql"
+			filec, // "202410251410_create_table_permissions.up.sql"
+			filed, // "202410251411_insert_permission_data_for_users.up.sql"
+			filee, // "202410251412_insert_permission_data_for_business.up.sql"
+			filef, // "202410251800_create_table_employees.up.sql"
+			fileg, // "202410251801_insert_employee_init_data.up.sql"
+			fileh, // "202410251802_create_table_employee_permissions.up.sql"
 
 		},
 	}
@@ -117,24 +138,27 @@ func init() {
 	// register embeddedBox
 	embedded.RegisterEmbeddedBox(`./migrations`, &embedded.EmbeddedBox{
 		Name: `./migrations`,
-		Time: time.Unix(1729842627, 0),
+		Time: time.Unix(1729861965, 0),
 		Dirs: map[string]*embedded.EmbeddedDir{
 			"": dir1,
 		},
 		Files: map[string]*embedded.EmbeddedFile{
-			"202410251400_create_table_status.up.sql":              file2,
-			"202410251401_insert_status_data.up.sql":               file3,
-			"202410251402_create_table_days.up.sql":                file4,
-			"202410251403_insert_days_data.up.sql":                 file5,
-			"202410251404_create_table_users.up.sql":               file6,
-			"202410251405_insert_users_init_data.up.sql":           file7,
-			"202410251406_create_table_business.up.sql":            file8,
-			"202410251407_create_table_business_configs.up.sql":    file9,
-			"202410251408_create_table_user_actions.up.sql":        filea,
-			"202410251409_create_table_user_permissions.sql":       fileb,
-			"202410251410_create_table_permissions.sql":            filec,
-			"202410251411_insert_permission_data_for_users.sql":    filed,
-			"202410251412_insert_permission_data_for_business.sql": filee,
+			"202410251400_create_table_status.up.sql":                 file2,
+			"202410251401_insert_status_data.up.sql":                  file3,
+			"202410251402_create_table_days.up.sql":                   file4,
+			"202410251403_insert_days_data.up.sql":                    file5,
+			"202410251404_create_table_users.up.sql":                  file6,
+			"202410251405_insert_users_init_data.up.sql":              file7,
+			"202410251406_create_table_business.up.sql":               file8,
+			"202410251407_create_table_business_configs.up.sql":       file9,
+			"202410251408_create_table_user_actions.up.sql":           filea,
+			"202410251409_create_table_user_permissions.sql":          fileb,
+			"202410251410_create_table_permissions.up.sql":            filec,
+			"202410251411_insert_permission_data_for_users.up.sql":    filed,
+			"202410251412_insert_permission_data_for_business.up.sql": filee,
+			"202410251800_create_table_employees.up.sql":              filef,
+			"202410251801_insert_employee_init_data.up.sql":           fileg,
+			"202410251802_create_table_employee_permissions.up.sql":   fileh,
 		},
 	})
 }
